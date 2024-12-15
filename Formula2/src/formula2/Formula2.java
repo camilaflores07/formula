22c1d5456a5bbb4a96f18baeb7818df2a509b51a
Camila Flores <camila_maria@unitec.edu>
2024-12-14 19:47:31 -0600

    Add files via upload

---
+++ Formula2/src/formula2/Formula2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package formula2;
+
+/**
+ *
+ * @author 50494
+ */
+public class Formula2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Pantalla2 pan1 = new Pantalla2();
+        pan1.setVisible(true);
+    }
+    
+}
